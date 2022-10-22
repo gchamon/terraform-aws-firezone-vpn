f048f679ffd4c1796cfc402b4180f765be30c36a
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-10-22 13:25:44 -0300

    update email validation module to use jq instead of python

--- config_gen.tf
+++ config_gen.tf
@@ -1,9 +1,11 @@
 module "validate_email" {
   source  = "rhythmictech/errorcheck/terraform"
-  version = "1.0.0"
+  version = "1.3.0"
 
-  assert = length(regexall("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$)", var.admin_user_email)) > 0
+  assert        = length(regexall("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$)", var.admin_user_email)) > 0
   error_message = "The email provided for the admin account is invalid: ${var.admin_user_email}"
+
+  use_jq = true
 }
 
 resource "random_password" "admin_password" {
