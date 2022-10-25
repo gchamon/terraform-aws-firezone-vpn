c5cdcb8cd042eb0b64699fcfd42d0d3475756565
Gabriel Chamon Araujo <gabriel.chamon@tutanota.com>
2022-10-25 12:33:42 -0300

    Fix/security group name and bucket module (#2)
    * security group name reflects module name; move bucket creation to module
    
    * add moved clause

--- s3.tf
+++ s3.tf
@@ -1,18 +1,29 @@
-resource "aws_s3_bucket" "backup" {
+module "backup_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.4.0"
+
   bucket_prefix = "${var.name}-backup-"
   acl           = "private"
 
-  versioning {
+  versioning = {
     enabled = true
   }
 
-  lifecycle_rule {
-    enabled = true
+  lifecycle_rule = [
+    {
+      enabled = true
+      id      = "expire-old-backups"
 
-    noncurrent_version_expiration {
-      days = 7
+      noncurrent_version_expiration = {
+        days = 7
+      }
     }
-  }
+  ]
 
   tags = var.tags
+}
+
+moved {
+  from = aws_s3_bucket.backup
+  to = module.backup_bucket.aws_s3_bucket.this
 }
